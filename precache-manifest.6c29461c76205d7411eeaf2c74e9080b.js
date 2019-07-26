9c33aeddb33ffa168a9b22820fa4c7fea6c66f98
unknown <fraj.mohamed97@gmail.com>
2019-07-26 16:26:38 +0100

    Updates

---
+++ precache-manifest.6c29461c76205d7411eeaf2c74e9080b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13e01850e8a05e7717da2d557bb8f97",
+    "url": "/stateful/index.html"
+  },
+  {
+    "revision": "6f4d734b48c1b926985c",
+    "url": "/stateful/static/css/main.2e34308d.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/stateful/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "6f4d734b48c1b926985c",
+    "url": "/stateful/static/js/main.750f1229.chunk.js"
+  },
+  {
+    "revision": "3a83249c6a07ebfb419f",
+    "url": "/stateful/static/js/runtime~main.bbddf002.js"
+  }
+]);
